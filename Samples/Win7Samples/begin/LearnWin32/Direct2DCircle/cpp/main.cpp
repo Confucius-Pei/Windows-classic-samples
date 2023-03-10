a2cfa38311d21a8f124075bfbc59bedbf999136e
peterpei <peterpei@tencent.com>
2023-03-10 14:31:10 +0800

    feat: Use WinRT XAML hosting API

--- Samples/Win7Samples/begin/LearnWin32/Direct2DCircle/cpp/main.cpp
+++ Samples/Win7Samples/begin/LearnWin32/Direct2DCircle/cpp/main.cpp
@@ -7,6 +7,23 @@
 #include "basewin.h"
 using namespace winrt::Windows::System::Diagnostics;
 
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.system.h>
+#include <winrt/windows.ui.xaml.hosting.h>
+#include <windows.ui.xaml.hosting.desktopwindowxamlsource.h>
+#include <winrt/windows.ui.xaml.controls.h>
+#include <winrt/Windows.ui.xaml.media.h>
+
+using namespace winrt;
+using namespace Windows::UI;
+using namespace Windows::UI::Composition;
+using namespace Windows::UI::Xaml::Hosting;
+using namespace Windows::Foundation::Numerics;
+
 template <class T> void SafeRelease(T **ppT)
 {
     if (*ppT)
@@ -138,6 +155,49 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
     {
         return 0;
     }
+
+    // Begin XAML Island section.
+
+// The call to winrt::init_apartment initializes COM; by default, in a multithreaded apartment.
+    winrt::init_apartment(apartment_type::single_threaded);
+
+    // Initialize the XAML framework's core window for the current thread.
+    WindowsXamlManager winxamlmanager = WindowsXamlManager::InitializeForCurrentThread();
+
+    // This DesktopWindowXamlSource is the object that enables a non-UWP desktop application 
+    // to host WinRT XAML controls in any UI element that is associated with a window handle (HWND).
+    DesktopWindowXamlSource desktopSource;
+
+    // Get handle to the core window.
+    auto interop = desktopSource.as<IDesktopWindowXamlSourceNative>();
+
+    // Parent the DesktopWindowXamlSource object to the current window.
+    check_hresult(interop->AttachToWindow(win.Window()));
+
+    // This HWND will be the window handler for the XAML Island: A child window that contains XAML.  
+    HWND hWndXamlIsland = nullptr;
+
+    // Get the new child window's HWND. 
+    interop->get_WindowHandle(&hWndXamlIsland);
+
+    // Update the XAML Island window size because initially it is 0,0.
+    SetWindowPos(hWndXamlIsland, 0, 20, 10, 80, 20, SWP_SHOWWINDOW);
+
+    // Create the XAML content.
+    Windows::UI::Xaml::Controls::StackPanel xamlContainer;
+    xamlContainer.Background(Windows::UI::Xaml::Media::SolidColorBrush{ Windows::UI::Colors::LightGray() });
+
+    Windows::UI::Xaml::Controls::TextBlock tb;
+    tb.Text(L"Hello World from Xaml Islands!");
+    tb.VerticalAlignment(Windows::UI::Xaml::VerticalAlignment::Center);
+    tb.HorizontalAlignment(Windows::UI::Xaml::HorizontalAlignment::Center);
+    tb.FontSize(48);
+
+    xamlContainer.Children().Append(tb);
+    xamlContainer.UpdateLayout();
+    desktopSource.Content(xamlContainer);
+
+    // End XAML Island section.
 
     ShowWindow(win.Window(), nCmdShow);
 
